4b864d9c9ae69fa35b94e59ae724e0dde4ae1041
tavisca-ssrivastava <ssrivastava@tavisca.com>
2019-06-12 15:19:34 +0530

    [add] test project for logging using platform-common

---
+++ Tavisca.Libraries.Logging.Debugging/LogWriterFactory.cs
@@ -0,0 +1,19 @@
+﻿using Tavisca.Common.Plugins.Configuration;
+using Tavisca.Common.Plugins.Redis;
+using Tavisca.Platform.Common.Logging;
+using Tavisca.Platform.Common.Plugins.Json;
+
+namespace Tavisca.Libraries.Logging.Debugging
+{
+    public class LogWriterFactory : ILogWriterFactory
+    {
+        public IApplicationLogWriter CreateWriter()
+        {
+            var configProvider = new ConfigurationProvider("exchange_rates_service");
+            var redisLogSettingsProvider = new RedisSettingsProvider(configProvider);
+            var redisSink = new RedisSink(redisLogSettingsProvider);
+
+            return new LogWriter(JsonLogFormatter.Instance, redisSink, null, configProvider);
+        }
+    }
+}
---
+++ Tavisca.Libraries.Logging.Debugging/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Tavisca.Platform.Common.Logging;
+
+namespace Tavisca.Libraries.Logging.Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logWriterFactory = new LogWriterFactory();
+            Logger.Initialize(logWriterFactory);
+
+            var log = new ApiLog
+            {
+                ApplicationName = "logging_test_app"
+            };
+
+            Logger.WriteLogAsync(log);
+
+            Console.ReadLine();
+        }
+    }
+}
